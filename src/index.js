8868b9d1a2eca57022c464369487ccfe431e6221
miskogepa <milovan.jankovic50m@gmail.com>
2025-05-14 06:44:05 +0200

    feat: implement MongoDB connection and update server initialization

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
+import { connect } from "mongoose";
+import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./.env",
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error:", error);
+    process.exit(1);
+  });
