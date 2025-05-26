55bfc6075c12153cd9885187768468cce28b8b15
miskogepa <milovan.jankovic50m@gmail.com>
2025-05-26 16:32:49 +0200

    feat: create video model with schema definition and export

---
+++ src/models/video.models.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // Path to the video file
+      required: true,
+      trim: true,
+    },
+
+    thumbnailUrl: {
+      type: String,
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+
+    duration: {
+      type: Number,
+      required: true,
+    },
+
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true } // Automatically adds createdAt and updatedAt fields
+);
+
+export const Video = mongoose.model("Video", videoSchema);
