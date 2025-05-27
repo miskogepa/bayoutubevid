9333e9280763df021472ef175f01523770164cd9
miskogepa <milovan.jankovic50m@gmail.com>
2025-05-27 08:36:09 +0200

    feat: add models for Comment, Like, Playlist, Subscription, and Tweet with schema definitions and exports

---
+++ src/models/playlist.models.js
@@ -0,0 +1,29 @@
+//Schema for Playlist Model
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
+// export default mongoose.model("Playlist", playlistSchema);
